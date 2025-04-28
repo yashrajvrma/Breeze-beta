6de4b606c58b68bb1f53aff122035dbd115ddc63
yashrajvrma <vermayashraj2005@gmail.com>
2025-04-29 01:33:07 +0530

    new path to client

--- db/index.ts
+++ db/index.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../node_modules/generated/prisma";
+import { PrismaClient } from "@/prisma/generated/client";
 
 const prismaClientSingleton = () => {
   return new PrismaClient();
